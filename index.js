a214d788bda6f34f2453251a1fb5a1489fd43822
Ataur Rahaman <ataurfb4445@gmail.com>
2025-08-30 15:52:29 +0600

    added listings creating functionality (post method)

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const listingCollection = client.db("roommateDB").collection("listings");
+
+    app.post("/listings", async(req, res) => {
+        const newListing = req.body;
+        console.log(newListing);
+        const result = await listingCollection.insertOne(newListing);
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
