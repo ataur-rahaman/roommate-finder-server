ca8a489add9ef169028ccb9827b7b272574a4c8b
Ataur Rahaman <ataurfb4445@gmail.com>
2025-08-31 16:54:01 +0600

    added delete api

--- index.js
+++ index.js
@@ -62,7 +62,14 @@ async function run() {
         res.send(result)
     })
 
-    app.put("listings/:id", async(req, res) => {
+    app.delete("/listings/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await listingCollection.deleteOne(query);
+      res.send(result)
+    })
+
+    app.put("/listings/:id", async(req, res) => {
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
       const updatedListing = req.body;
@@ -73,6 +80,10 @@ async function run() {
       const result = await listingCollection.updateOne(filter, updatedDoc);
       res.send(result);
     })
+
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
